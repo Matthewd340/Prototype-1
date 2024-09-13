c0d4b3fcf503ff07a48e8ba5416304071282da9e
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2024-09-13 12:27:44 -0400

    Added collision and more obstacles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moves the vehicle foward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
         
     }
 }
