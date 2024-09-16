935e6dadff5c9031869a6eb00a0062c7142851c4
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2024-09-16 12:33:56 -0400

    Made the camera follow the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //allows speed to be edited form inspector
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
