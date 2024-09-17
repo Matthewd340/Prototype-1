bb78e6c7d149732da9d39dbf8a82203ea63b5d59
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2024-09-17 12:39:27 -0400

    Added player control to the movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //get horizontal and forward inputs for steering
+    float horizontalInput;
+    float forwardInput;
     //allows speed to be edited form inspector
     public float speed = 5.0f;
+    public float turnSpeed = 5.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Moves the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       
+        //Get axes for vehicle control
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
         
     }
 }
